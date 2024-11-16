8d94746ee27d43f57549c661db93fe42a41e2eb5
etan675 <erictang1201@gmail.com>
2024-11-16 15:42:15 +1100

    set up project + main page

---
+++ src/components/Content.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames'
+import React, { ReactNode } from 'react'
+
+type Props = Readonly<{
+    className?: string
+    children: ReactNode
+}>
+
+const Content = ({ className, children }: Props) => {
+  return (
+    <div className={classNames(
+        'w-[1140px] max-w-[80%] m-auto',
+        className
+    )}>
+        {children}
+    </div>
+  )
+}
+
+export default Content
+
---
+++ src/components/TopBanner.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import React from 'react'
+import SiteLogo from './SiteLogo'
+import classNames from 'classnames'
+
+type Props = Readonly<{ 
+    className?: string 
+}>
+
+const TopBanner = ({ className }: Props) => {
+  return (
+    <div className={classNames(
+        'w-full h-20 py-5 px-8 flex items-center',
+        className
+    )}>
+        <Link className='flex items-center gap-2 h-full' href={'/'}>
+            <SiteLogo fill='white' width={30} height={30} />
+            <span className='text-3xl font-black'>Tracker.GG</span>
+        </Link>
+    </div>
+  )
+}
+
+export default TopBanner
